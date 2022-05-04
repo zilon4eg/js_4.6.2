39afe7abc3bbb0df6bc6af9d2c8b7241e1feb1db
Igor <hdd500t@yandex.ru>
2022-05-04 22:42:32 +0300

    complete

--- task.js
+++ task.js
@@ -1,26 +1,30 @@
 //выбираем нужные элементы
 const menu = document.querySelectorAll('.menu__link');
-
-// console.log(menu[1]);
-// menu[1].className('menu menu_sub menu_active');
-
-// const menuParant = menu[1].closest('.menu__item');
-// const menuSub = 
-// console.log(`nfnf ${menuSub}`);
-const menuSub = document.querySelectorAll('.menu_main li');
-console.log(menuSub);
-
-
-// var a = document.querySelectorAll('.link');
     
 //перебираем все найденные элементы и вешаем на них события
 menu.forEach(function(el) {
     //вешаем событие
-    // console.log(el);
     el.onclick = function() {
+
         //производим действия
-        // console.log(menu[el]);
-        el.className('menu menu_sub menu_active');
+        console.log(el);
+
+        let menuParant = el.closest('.menu__item');
+        let menuSub = menuParant.querySelector('.menu_sub');
+        if (menuSub.className.includes('menu_active')) {
+            menuSub.className = 'menu menu_sub';
+        }
+        else {
+            menuSub.className = 'menu menu_sub menu_active';
+        }
+
+        let menuAllSub = document.querySelectorAll('.menu_sub');
+        menuAllSub.forEach(function(elSub) {
+            if (menuSub != elSub) {
+                elSub.className = 'menu menu_sub';
+            }
+        });
+
         //предотвращаем переход по ссылке href
         return false;
     }
